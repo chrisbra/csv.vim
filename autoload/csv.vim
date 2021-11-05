chrisbra/csv.vim

commit e1f1c890f524b697ca82a66820ff94dfafb81d56

arrangecol: do not grow width everytime
fixes #146
fixes #160

autoload/csv.vim
@@ -898,7 +898,7 @@ fu! csv#Columnize(field) "{{{3
         return result
     else
         " right align
-        return printf("%*S", width+1 ,  a:field)
+        return printf("%*S", width ,  a:field)
     endif
 endfun
 fu! csv#GetColPat(colnr, zs_flag) "{{{3

commit 9ab092187a7046a8d0eedc7fbbce8fac38d10563

Tabularize: correctly calculate marginline
Unfortunately, commit e1f1c890f524b697ca82a broke the calculation of the
marginline/headerline when creating tables.

So let's fix it, by correctly calculating the width and using one space
less, becasue we need to add the little corsbars ┼ or '+' between each column
(marked by the horizontal line '-'.

fixes #200

autoload/csv.vim
@@ -2720,7 +2720,7 @@ fu! csv#Tabularize(bang, first, last) "{{{3
     if getline(a:first)[-1:] isnot? b:delimiter
         let b:col_width[-1] += 1
     endif
-    let marginline = s:td.scol. join(map(copy(b:col_width), 'repeat(s:td.hbar, v:val)'), s:td.cros). s:td.ecol
+    let marginline = s:td.scol. join(map(copy(b:col_width), 'repeat(s:td.hbar, v:val-1)'), s:td.cros). s:td.ecol
 
     call csv#NewDelimiter(s:td.vbar, 1, line('$'))
     "exe printf('sil %d,%ds/%s/%s/ge', a:first, (a:last+adjust_last),